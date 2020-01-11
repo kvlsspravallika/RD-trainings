a3641b81bf754af1a51a97de79f7cca150c8f198
Kvlss Pravallika <kvlss.pravallika@gmail.com>
2020-01-11 12:13:49 +0530

    changes committed for naming-task2

--- CleanCode/clean-code-tasks-java-master/Naming/src/main/java/com/epam/engx/cleancode/naming/task2/User.java
+++ CleanCode/clean-code-tasks-java-master/Naming/src/main/java/com/epam/engx/cleancode/naming/task2/User.java
@@ -5,25 +5,25 @@ import java.util.Date;
 
 public class User {
 
-	private Date dBirth;
+	private Date dateOfBirth;
 
-	private String sName;
+	private String name;
 
-	private boolean bAdmin;
+	private boolean isAdmin;
 
-	private User[] subordinateArray;
+	private User[] users;
 
-	private int iR;
+	private int rating;
 
-	public User(String sName) {
+	public User(String name) {
 		super();
-		this.sName = sName;
+		this.name = name;
 	}
 
 	@Override
 	public String toString() {
-		return "User [dBirth=" + dBirth + ", sName=" + sName + ", bAdmin=" + bAdmin + ", subordinateArray="
-				+ Arrays.toString(subordinateArray) + ", iRating=" + iR + "]";
+		return "User [dateOfBirth=" + dateOfBirth + ", Name=" + name + ", isAdmin=" + isAdmin + ", Users="
+				+ Arrays.toString(users) + ", Rating=" + rating + "]";
 	}
 
 }
